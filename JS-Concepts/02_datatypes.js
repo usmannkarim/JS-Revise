5b8ea94ed1a72627e6d4edfdd3ef9d8f2b8336b3
Muhammad Usman Karim <96872170+usmannkarim@users.noreply.github.com>
2023-12-05 07:58:26 +0000

    datatypes

---
+++ JS-Concepts/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all below JS code as a newer version of JS
+
+// alert("hello") // we are using node JS, not browser
+
+console.log(3+3); console.log("4") // not a good practise as readability is degrded
+ 
+// DATATYPES:
+// PRIMITIVE DATATYPES
+let name = "Ali" // string DT (preferred in double quotes)
+
+let age = 18  // Number DT (usually ranges from 2 to 2^53)
+
+// BigInt Dt (for no. greater than 2^53)
+
+//isLoggedIn = True // boloean (True/False)
+
+let state = null// null (yet along with being a DT it is a standalone value in JS) 
+/*NOTE: NULL IS NOT 0 !*/
+
+let newer; // undefine DT used to represent empty value (declared but not initialized values)
+
+//symbol used for uniqueness
+
+//NON-PRIMITIVE DATATYPES
+//Object i.e
+console.log(typeof null); // Null is of type object
+console.log(typeof undefine); // undefine is of type undefined
