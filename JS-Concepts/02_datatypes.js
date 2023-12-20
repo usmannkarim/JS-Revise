bf9c680f5ed621d84c3537f4d5293cb0cc620603
Muhammad Usman Karim <96872170+usmannkarim@users.noreply.github.com>
2023-12-20 12:52:17 +0000

    20-Dec-Update

--- JS-Concepts/02_datatypes.js
+++ JS-Concepts/02_datatypes.js
@@ -2,14 +2,14 @@
 
 // alert("hello") // we are using node JS, not browser
 
-console.log(3+3); console.log("4") // not a good practise as readability is degrded
+// console.log(3+3); console.log("4") // not a good practise as readability is degrded
  
 // DATATYPES:
 // PRIMITIVE DATATYPES
 let name = "Ali" // string DT (preferred in double quotes)
 
-let age = 18  // Number DT (usually ranges from 2 to 2^53)
-
+let age = '18avd'  // Number DT (usually ranges from 2 to 2^53)
+let age_group = Number(age)
 // BigInt Dt (for no. greater than 2^53)
 
 //isLoggedIn = True // boloean (True/False)
@@ -23,5 +23,34 @@ let newer; // undefine DT used to represent empty value (declared but not initia
 
 //NON-PRIMITIVE DATATYPES
 //Object i.e
-console.log(typeof null); // Null is of type object
-console.log(typeof undefine); // undefine is of type undefined
+// console.log(typeof null); // Null is of type object
+// console.log(typeof undefine); // undefine is of type undefined
+// console.log(typeof age_group); // value sometines converted but sometimes not i.e
+// '33' -> 33, number
+// '33abc' -> 33abc, NaN
+// true -> 1 ; false -> 0;
+// console.log(age_group) 
+
+//
+
+
+let isLoggedIn = 1
+let issessionOn = Boolean(isLoggedIn)
+
+// console.log (issessionOn)
+// console.log (typeof issessionOn)
+
+// 1 -> true
+// 0 -> false
+// "" -> false
+// "xyz.." -> true
+
+//
+let anumber = "0";
+let astring = String(anumber)
+console.log(astring);   
+console.log(typeof astring);
+
+// "12" -> string
+// "" -> string
+// "acsa.." -> string
